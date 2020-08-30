3fa3c003224835d4013f9efa40f450a2867ac226
VanessaNicole1 <nicolegualan@gmail.com>
2020-08-30 16:00:05 -0500

    Saludo loop

---
+++ saludoloop.asm
@@ -0,0 +1,25 @@
+; Vanessa Nicole Iniguez
+section .data
+    saludo db "Hola mundo",10
+    len_saludo equ $-saludo
+
+section .txt
+    global _start
+
+_start:
+    mov ecx, 20
+
+for:
+    push ecx
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, saludo
+    mov edx, len_saludo
+    int 80h
+    pop ecx
+    
+    loop for
+
+salir:
+    mov eax, 1
+    int 80h 
